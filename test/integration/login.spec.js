a4ed23044ee488a2bce31160a3b44c7ee863a5fb
Anna Trzeciak <trzeciakanna@hotmail.com>
2020-07-27 20:03:59 +0200

    Added tests to login page

---
+++ test/integration/login.spec.js
@@ -0,0 +1,40 @@
+describe("Login page", () => {
+  it("Login into account - load login form", () => {
+    cy.visit("http://gl-calc.docker:8080/zaloguj");
+    cy.get("input").should("have.length", 2);
+    cy.get("input#input-email").should("have.length", 1);
+    cy.get("input#input-password").should("have.length", 1);
+    cy.get("button[type=submit]").should("have.length", 1);
+    cy.get("button[type=submit]").should("be.disabled");
+    cy.get("form>p> a")
+      .should("have.length", 1)
+      .should("have.text", "Załóż je");
+  });
+
+  it("Login into account as admin", () => {
+    cy.visit("http://gl-calc.docker:8080/zaloguj");
+    cy.get("input#input-email").type("admin@admin.com");
+    cy.get("input#input-password").type("admin");
+    cy.get("button").click();
+    cy.get("div.details").should("contain", "admin@admin.com");
+  });
+
+  it("Login into account - wrong pass", () => {
+    cy.visit("http://gl-calc.docker:8080/zaloguj");
+    cy.get("input#input-email").type("admin@admin.com");
+    cy.get("input#input-password").type("admin1");
+    cy.get("button").click();
+    cy.get("div.alert-danger").should("contain", "Niepoprawne hasło");
+  });
+
+  it("Login into account - wrong email", () => {
+    cy.visit("http://gl-calc.docker:8080/zaloguj");
+    cy.get("input#input-email").type("admin-wrong@admin.com");
+    cy.get("input#input-password").type("admin");
+    cy.get("button").click();
+    cy.get("div.alert-danger").should(
+      "contain",
+      "Nie znaleziono użytkownika o podanym adresie email"
+    );
+  });
+});
