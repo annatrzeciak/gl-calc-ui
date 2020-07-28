aa25b440e71de0002333e0dfe9cab21b4c02d833
Anna Trzeciak <trzeciakanna@hotmail.com>
2020-07-28 19:54:59 +0200

    Added registration tests.

--- test/integration/login.spec.js
+++ test/integration/login.spec.js
@@ -1,6 +1,6 @@
 describe("Login page", () => {
   it("Login into account - load login form", () => {
-    cy.visit("http://gl-calc.docker:8080/zaloguj");
+    cy.visit("/zaloguj");
     cy.get("input").should("have.length", 2);
     cy.get("input#input-email").should("have.length", 1);
     cy.get("input#input-password").should("have.length", 1);
@@ -12,7 +12,7 @@ describe("Login page", () => {
   });
 
   it("Login into account as admin", () => {
-    cy.visit("http://gl-calc.docker:8080/zaloguj");
+    cy.visit("/zaloguj");
     cy.get("input#input-email").type("admin@admin.com");
     cy.get("input#input-password").type("admin");
     cy.get("button").click();
@@ -20,7 +20,7 @@ describe("Login page", () => {
   });
 
   it("Login into account - wrong pass", () => {
-    cy.visit("http://gl-calc.docker:8080/zaloguj");
+    cy.visit("/zaloguj");
     cy.get("input#input-email").type("admin@admin.com");
     cy.get("input#input-password").type("admin1");
     cy.get("button").click();
@@ -28,7 +28,7 @@ describe("Login page", () => {
   });
 
   it("Login into account - wrong email", () => {
-    cy.visit("http://gl-calc.docker:8080/zaloguj");
+    cy.visit("/zaloguj");
     cy.get("input#input-email").type("admin-wrong@admin.com");
     cy.get("input#input-password").type("admin");
     cy.get("button").click();
